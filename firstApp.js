abd9f8dddcd70e847bea82b24ffb649b662215f4
Pierluigi Causio <145658214+ST0MP3R9@users.noreply.github.com>
2025-04-11 13:19:00 +0200

    Semplificazione

--- firstApp.js
+++ firstApp.js
@@ -1,56 +1,50 @@
-const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
-const http = require('node:http');
+//const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
+//const http = require('node:http');
+const mqtt=require('mqtt');
+var client =
+	mqtt.connect("mqtt://mqtt.eclipseprojects.io",{clientId:"mqttjs01"});
+client.on("connect",function(){
+	console.log("connected");
+});
+client.on("error",function(error){
+	console.log("Can't connect"+error);
+});
 
 // Setup sensor, exit if failed
-var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
-var sensorPin = 4; // The GPIO pin number for sensor signal
-if (!sensorLib.initialize(sensorType, sensorPin))
+//var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
+//var sensorPin = 4; // The GPIO pin number for sensor signal
+/*if (!sensorLib.initialize(sensorType, sensorPin))
 {
 //print a warning message in the console
 console.warn('Failed to initialize sensor');
 process.exit(1);
-}
+}*/
 
 // Automatically update sensor value every 2 seconds
 //we use a nested function (function inside another function)
 setInterval(function() {
-	var readout = sensorLib.read();
+	//var readout = sensorLib.read();
 
-	console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-	console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
-	var temperature = readout.temperature.toFixed(1);
+	//console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
+	//console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
+	//var temperature = readout.temperature.toFixed(1);
 
 	const postData = JSON.stringify({
 		'sensor': 'ID1',
 		'timestamp': 12345678,
-		'temperature': temperature
+		'temperature': Math.random()
 	})
 	const options = {
-	hostname: '192.168.137.1',
-	port: 3000,
-	path: '/temperature',
-	method: 'POST',
-	headers: {
-	      'Content-Type': 'application/json',
-	      'Content-Length': Buffer.byteLength(postData),
-	},
+		hostname: 'localhost',
+		port: 3000,
+		path: '/temperature',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': Buffer.byteLength(postData),
+		},
 	};
 
-	const req = http.request(options, (res) => {
-		res.setEncoding('utf8');
-		res.on('data', (chunk) => {
-		  console.log(`BODY: ${chunk}`);
-		});
-		res.on('end', () => {
-		  console.log('No more data in response.');
-		});
-	});
+	client.publish("test-topic-handson/data", postData);
 
-	req.on('error', (e) => {
-		console.error(`problem with request: ${e.message}`);
-	});
-
-	// Write data to request body
-	req.write(postData);
-	req.end();
-}, 2000);
+}, 2000);
